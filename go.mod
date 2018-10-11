81c1d0a77902db5b1f1e02e46eb3ef884e7ac0fc
Alexey Naidyonov <alexey.naidyonov@itoolabs.com>
2018-10-11 10:41:14 +0300

    [WIP]: add keepalived events processing to watch

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway v1.5.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jonboulle/clockwork v0.1.0 // indirect
+	github.com/mattn/go-shellwords v1.0.3
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/prometheus/client_golang v0.8.0 // indirect
 	github.com/prometheus/client_model v0.0.0-20180712105110-5c3871d89910 // indirect
--- watch.go
+++ watch.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"errors"
 	"fmt"
 	"github.com/golang/snappy"
+	"github.com/mattn/go-shellwords"
 	"github.com/spf13/cobra"
 	"go.etcd.io/etcd/clientv3"
 	"io"
@@ -18,6 +20,14 @@ import (
 	"strings"
 	"sync"
 	"syscall"
+	"time"
+)
+
+var (
+	watchPrefix string
+	keepalivedFifo string
+	keepalivedPrefix string
+	keepalivedInstance string
 )
 
 func newWatchCommand() *cobra.Command {
@@ -30,13 +40,21 @@ certain service)
 
 watch command also performs initial synchronisation and runs command if any file has been updated.
 
+watch command may also listens for keepalived (http://www.keepalived.org) events FIFO and updates
+keepalived state in the etcd store 
+
 Example:
-confsync watch -- /etc/firewall/keepalived /services/keepalived/config sv restart keepalived 
+confsync watch --prefix /etc/firewall --ka-fifo /run/ka --ka-instance master --ka-key state \
+      -- keepalived /services/keepalived/config sv reload keepalived 
 
 `,
 		RunE: watchCommandFunc,
 		Args: cobra.MinimumNArgs(3),
 	}
+	cmd.Flags().StringVar(&watchPrefix, "prefix", "", "common `key` prefix for all watches and keepalived status")
+	cmd.Flags().StringVar(&keepalivedFifo, "ka-fifo", "", "`path` to keepalived events FIFO")
+	cmd.Flags().StringVar(&keepalivedInstance, "ka-instance", "", "keepalived instance `name`")
+	cmd.Flags().StringVar(&keepalivedPrefix, "ka-key", "", "`key` prefix to store keepalived status (joined with --prefix, if set)")
 	return cmd
 }
 
@@ -200,7 +218,7 @@ func watchCommandFunc(cmd *cobra.Command, args []string) error {
 			return fmt.Errorf("error finding command %s: %s", args[2], err)
 		}
 		watcher := &watcher{
-			prefix: args[0],
+			prefix: filepath.Join("/", watchPrefix, args[0]),
 			root:   args[1],
 			cmd:    cmd,
 		}
@@ -215,14 +233,96 @@ func watchCommandFunc(cmd *cobra.Command, args []string) error {
 		watcher.args = args[2:]
 		break
 	}
+	if keepalivedFifo != "" && keepalivedInstance == "" {
+		return fmt.Errorf("--ka-instance name must be set for processing keepalived events")
+	} else if keepalivedInstance != "" && keepalivedFifo == "" {
+		return fmt.Errorf("--ka-fifo name must be set for processing keepalived instance %s events", keepalivedInstance)
+	} else if keepalivedFifo != "" && keepalivedInstance != "" {
+		if watchPrefix != "" {
+			keepalivedPrefix = filepath.Join("/", watchPrefix, keepalivedPrefix)
+		}
+	}
 	return runWatchers(watchers)
+}
+
+func updateKeepalivedStatus(c *clientv3.Client, kind, instance, state string) {
+	var (
+		ops []clientv3.Op
+	)
+	ops = append(ops, clientv3.OpPut(filepath.Join(keepalivedPrefix, keepalivedInstance, kind, instance), state))
+	ckey := filepath.Join(keepalivedPrefix, "current", kind, instance)
+	if state == "MASTER" {
+		ops = append(ops, clientv3.OpPut(ckey, keepalivedInstance))
+	} else {
+		ops = append(ops, clientv3.OpTxn(
+			[]clientv3.Cmp{clientv3.Compare(clientv3.Value(ckey), "=", keepalivedInstance)},
+			[]clientv3.Op{clientv3.OpDelete(ckey)},
+			[]clientv3.Op{},
+		))
+	}
+	_, err := c.Txn(context.Background()).If().Then(ops...).Commit()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error updating keepalived status: %s", err)
+	}
+}
+
+func runKeepaliveStateUpdater(c *clientv3.Client, wg *sync.WaitGroup, stop chan struct{}) {
+	var (
+		events = make(chan string)
+		parser = shellwords.NewParser()
+	)
+	go runKeepalivedEventsListener(events)
+Outer:
+	for {
+		select {
+		case <-stop:
+			break Outer
+		case line := <-events:
+			if args, err := parser.Parse(line); err != nil {
+				fmt.Fprintf(os.Stderr, "error parsing keepalived event string %s: %s\n", line, err)
+			} else if len(args) < 3 {
+				fmt.Fprintf(os.Stderr, "error parsing keepalived event string %s: not enought parameters\n", line)
+			} else {
+				updateKeepalivedStatus(c, args[0], args[1], args[2])
+			}
+		}
+	}
+	wg.Done()
+}
+
+func runKeepalivedEventsListener(events chan string) {
+	var wt *time.Timer
+	for {
+		if wt != nil {
+			<-wt.C
+		}
+		fd, err := os.OpenFile(keepalivedFifo, os.O_RDONLY, 0)
+		if err != nil {
+			if wt == nil {
+				wt = time.NewTimer(1 * time.Second)
+			} else {
+				wt.Reset(1 * time.Second)
+			}
+		} else {
+			rdr := bufio.NewScanner(fd)
+			for rdr.Scan() {
+				events <- rdr.Text()
+			}
+			fd.Close()
+		}
+	}
 }
 
 func runWatchers(w []*watcher) error {
 	c := mustClient()
 	wg := &sync.WaitGroup{}
+	dc := make(chan struct{})
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGHUP, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
+	if keepalivedFifo != "" {
+		wg.Add(1)
+		go runKeepaliveStateUpdater(c, wg, dc)
+	}
 	for i := range w {
 		wg.Add(1)
 		go w[i].run(c, wg)
@@ -242,6 +342,7 @@ Loop:
 		}
 	}
 	signal.Stop(sc)
+	close(dc)
 	err := c.Close()
 	wg.Wait()
 	return err
